d444820348d2f9c9c3c047dc392b7da4dc2575be
Wosco <30988464+Wosco@users.noreply.github.com>
2017-08-18 23:29:54 -0400

    Quick timer on schedule
    Test out crontab reboot.

--- TrySchedule.py
+++ TrySchedule.py
@@ -4,8 +4,10 @@ import time
 def job():
 	print("It's working?")
 
-schedule.every().day.at("15:00").do(job)
+# schedule.every().day.at("15:00").do(job)
+
+schedule.every(10).seconds.do(job)
 
 while True:
 	schedule.run_pending()
-	time.sleep(60)
+	time.sleep(1)
