3bc258d537abd14e8b606584ff8ce5079bf06c9e
Wosco <test@test.com>
2017-08-19 15:52:55 -0400

    no more function calls

--- oneshot.py
+++ oneshot.py
@@ -1,3 +1,59 @@
-from rundaily import readAndWrite
+#from rundaily import readAndWrite
 
-readAndWrite
+#readAndWrite
+
+import pandas as pd
+import time
+
+mainData=pd.read_pickle("mainPickle.pkl")
+
+url = "https://coinmarketcap.com/all/views/all/"
+
+
+# This is just my normal dataframe prep from the big history read code
+dfs = pd.read_html(url)
+    
+df=dfs[0]
+
+# Today's date
+df=df.assign(Date=pd.to_datetime('today'))
+
+# Rename some headers
+df.rename(columns={df.columns[3]:'Cap',df.columns[5]:'Supply',df.columns[6]:'Vol'},inplace=True)
+
+# Convert market cap to float
+df['Cap']=df['Cap'].replace('[\$,]', '', regex=True).replace('?','0').astype(float)
+
+# Convert price to float
+df['Price']=df['Price'].replace('[\$,]', '', regex=True).replace('?','0').astype(float)
+
+# Convert supply to float
+df['Supply']=df['Supply'].replace('[\$, *]', '', regex=True).replace('?','0').astype(float)
+
+# Convert volume to float
+df['Vol']=df['Vol'].replace('[\$, *]', '', regex=True).replace('LowVol','0').replace('?','0').astype(float)
+
+# Add it on top of the big data read in
+comboDF=mainData.append(df, ignore_index=True)
+
+# Pivot the table for Date on x axis        
+capDF=comboDF.pivot_table(index='Date',columns='Name',values='Cap')
+
+# Fill in the gaps
+capDF.fillna(0, inplace=True)
+
+# Do same for Volume
+volDF=comboDF.pivot_table(index='Date',columns='Name',values='Vol')
+volDF.fillna(0,inplace=True)
+
+# Rank cap and volume just because
+capRank=capDF.rank(axis=1,method='min',ascending=False)
+volRank=volDF.rank(axis=1,method='min',ascending=False)
+
+# Dump to file
+comboDF.to_pickle("mainPickle.pkl")
+
+capDF.to_csv("cap.csv")
+volDF.to_csv("vol.csv")
+capRank.to_csv("capRank.csv")
+volRank.to_csv("volRank.csv")
